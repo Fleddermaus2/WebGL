c61f22744be6bba5343f23f21b0053a5ee71c832
Fleddermaus <fleddermaus2@gmail.com>
2016-09-15 13:15:21 +0200

    cube nebular

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ var gl;
 var cubeVertexPositionBuffer;
 //buffer for objectTexture coordinates
 var cubeVertexTextureCoordBuffer;
-//buffer for normals
-var cubeVertexNormalBuffer;
+//buffer for indices
+var cubeVertexIndexBuffer;
 
 //model-view-matrix
 var mvMatrix = mat4.identity(mat4.create());
@@ -36,7 +36,7 @@ var objectTexture;
 var currentlyPressedKey = {};
 
 //objects
-var stars = [];
+var cubes = [];
 var mvMatrixStack = [];
 
 //initialise gl
@@ -139,7 +139,7 @@ function initTexture() {
         handleLoadedTexture(objectTexture);
     };
 
-    objectTexture.image.src = "star.gif";
+    objectTexture.image.src = "crate.gif";
 }
 
 function mvPushMatrix() {
@@ -200,33 +200,94 @@ function initBuffers() {
     gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
 
     var vertices = [
-        -1.0, -1.0,  0.0,
-        1.0, -1.0,  0.0,
-        -1.0,  1.0,  0.0,
-        1.0,  1.0,  0.0
+        // vordere Fläche
+        -1.0, -1.0,  1.0, //0
+        1.0, -1.0,  1.0, //1
+        1.0,  1.0,  1.0,
+        -1.0,  1.0,  1.0,
+
+        // hintere Fläche
+        -1.0, -1.0, -1.0, //4
+        -1.0,  1.0, -1.0, //5
+        1.0,  1.0, -1.0,
+        1.0, -1.0, -1.0,
+
+        // obere Fläche
+        -1.0,  1.0, -1.0, //8
+        -1.0,  1.0,  1.0, //9
+        1.0,  1.0,  1.0,
+        1.0,  1.0, -1.0,
+
+        // untere Fläche
+        -1.0, -1.0, -1.0, //12
+        1.0, -1.0, -1.0, //13
+        1.0, -1.0,  1.0,
+        -1.0, -1.0,  1.0,
+
+        // rechte Fläche
+        1.0, -1.0, -1.0, //16
+        1.0,  1.0, -1.0, //17
+        1.0,  1.0,  1.0,
+        1.0, -1.0,  1.0,
+
+        // linke Fläche
+        -1.0, -1.0, -1.0, //20
+        -1.0, -1.0,  1.0, //21
+        -1.0,  1.0,  1.0,
+        -1.0,  1.0, -1.0
     ];
 
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
     cubeVertexPositionBuffer.itemSize = 3;
-    cubeVertexPositionBuffer.numItems = 4;
+    cubeVertexPositionBuffer.numItems = 24;
 
-    //create cube texturecoord buffer
+    //create cube texture buffer
     cubeVertexTextureCoordBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexTextureCoordBuffer);
 
     var textureCoords = [
+        // Front face
         0.0, 0.0,
         1.0, 0.0,
+        1.0, 1.0,
         0.0, 1.0,
-        1.0, 1.0
+
+        // Back face
+        1.0, 0.0,
+        1.0, 1.0,
+        0.0, 1.0,
+        0.0, 0.0,
+
+        // Top face
+        0.0, 1.0,
+        0.0, 0.0,
+        1.0, 0.0,
+        1.0, 1.0,
+
+        // Bottom face
+        1.0, 1.0,
+        0.0, 1.0,
+        0.0, 0.0,
+        1.0, 0.0,
+
+        // Right face
+        1.0, 0.0,
+        1.0, 1.0,
+        0.0, 1.0,
+        0.0, 0.0,
+
+        // Left face
+        0.0, 0.0,
+        1.0, 0.0,
+        1.0, 1.0,
+        0.0, 1.0
     ];
 
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
     cubeVertexTextureCoordBuffer.itemSize = 2;
-    cubeVertexTextureCoordBuffer.numItems = 4;
+    cubeVertexTextureCoordBuffer.numItems = 24;
 
-    /*
     //index buffer
     cubeVertexIndexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
@@ -240,84 +301,71 @@ function initBuffers() {
     ];
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
     cubeVertexIndexBuffer.itemSize = 1;
-    cubeVertexIndexBuffer.numItems = 36;*/
+    cubeVertexIndexBuffer.numItems = 36;
 }
 
-function drawStar() {
+function drawCube() {
     gl.activeTexture(gl.TEXTURE0);
     gl.bindTexture(gl.TEXTURE_2D, objectTexture);
     gl.uniform1i(shaderProgram.samplerUniform, 0);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexTextureCoordBuffer);
-    gl.vertexAttribPointer(shaderProgram.vertexTextureCoordAttribute, cubeVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
-
     gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
     gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, cubeVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
+    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexTextureCoordBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexTextureCoordAttribute, cubeVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
     setMatrixUniforms();
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, cubeVertexPositionBuffer.numItems);
+    gl.drawElements(gl.TRIANGLES, cubeVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
 }
 
-function Star(startingDistance, rotationSpeed) {
+function Cube(startingDistance, rotationSpeed) {
     this.angle = 0;
     this.dist = startingDistance;
     this.rotationSpeed = rotationSpeed;
     //set colors to a starting value
     this.randomiseColors();
 }
-Star.prototype.draw = function (tilt, spin, twinkle) {
+Cube.prototype.draw = function (tilt, spin) {
     mvPushMatrix();
 
-    //move to stars position
+    //move to cubes position
     mat4.rotate(mvMatrix, mvMatrix, degToRad(this.angle), [0.0, 1.0, 0.0]);
     mat4.translate(mvMatrix, mvMatrix, [this.dist, 0.0, 0.0]);
 
-    //rotate back so that the star is facing the viewer
-    mat4.rotate(mvMatrix, mvMatrix, degToRad(-this.angle), [0.0, 1.0, 0.0]);
-    mat4.rotate(mvMatrix, mvMatrix, degToRad(-tilt), [1.0, 0.0, 0.0]);
-
-    if(twinkle){
-        gl.uniform3f(shaderProgram.colorUniform, this.twinkleR, this.twinkleG, this.twinkleB);
-        drawStar();
-    }
-
-    //stars spin around z at the same rate
+    //cubes spin around z at the same rate
     mat4.rotate(mvMatrix, mvMatrix, degToRad(spin), [0.0, 0.0, 1.0]);
 
     //draw star in main color
     gl.uniform3f(shaderProgram.colorUniform, this.r, this.g, this.b);
-    drawStar();
+    drawCube();
 
     mvPopMatrix();
 };
 
-Star.prototype.animate = function (elapsedTime) {
+Cube.prototype.animate = function (elapsedTime) {
     this.angle += this.rotationSpeed * effectiveFPMS * elapsedTime;
     //decrease the distance, resetting the star to the outside of the spiral if it is at the center
     this.dist -= 0.01 * effectiveFPMS * elapsedTime;
-    if(this.dist < 0.0){
-        this.dist += 5.0;
+    if(this.dist < 5.0){
+        this.dist += 15.0;
         this.randomiseColors();
     }
 };
 
-Star.prototype.randomiseColors = function () {
+Cube.prototype.randomiseColors = function () {
     //give star color
     this.r = Math.random();
     this.g = Math.random();
     this.b = Math.random();
-
-    //twinkle
-    this.twinkleR = Math.random();
-    this.twinkleG = Math.random();
-    this.twinkleB = Math.random();
 };
 
 function initWorldObjects() {
-    var numStars = 50;
+    var numStars = 100;
 
     for(var i = 0; i < numStars; i++){
-        stars.push(new Star((i/numStars) * 5.0, i/numStars));
+        cubes.push(new Cube((i/numStars) * 5.0 + 10.0, i/numStars));
     }
 }
 
@@ -327,19 +375,15 @@ function drawScene() {
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     mat4.perspective(pMatrix, 45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0);
-    //switch to blending
-    gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
-    gl.enable(gl.BLEND);
 
     mat4.identity(mvMatrix);
     mat4.translate(mvMatrix, mvMatrix, [0.0, 0, z]);
     //rotate
     mat4.rotate(mvMatrix, mvMatrix, degToRad(tilt), [1, 0, 0]);
 
-    var twinkle = document.getElementById("twinkle").checked;
-    for(var i in stars){
-        stars[i].draw(tilt, spin, twinkle);
-        spin += 0.1;
+    for(var i in cubes){
+        cubes[i].draw(tilt, spin);
+        spin += 0.001;
     }
 }
 
@@ -350,8 +394,8 @@ function animate() {
     if(lastTime != 0){
         var elapsed = timeNow - lastTime;
 
-        for(var i in stars){
-            stars[i].animate(elapsed);
+        for(var i in cubes){
+            cubes[i].animate(elapsed);
         }
     }
 
@@ -375,9 +419,8 @@ function webGL() {
     initWorldObjects();
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    //depth testing and blending don't work well together
-    //gl.enable(gl.DEPTH_TEST);
-    //gl.depthFunc(gl.LEQUAL);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
 
     document.onkeydown = handleKeyDown;
     document.onkeyup = handleKeyUp;
